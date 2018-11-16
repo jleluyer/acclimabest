2ad97539c34a47f0673165ba5fd0aaff9dc3734e
jleluyer <jeremy.le.luyer@gmail.com>
2018-11-15 17:34:56 -1000

    Update 01_trimmomatic_jobs_pe.sh

--- 00_scripts/datarmor_jobs/02_diff_expression/01_trimmomatic_jobs_pe.sh
+++ 00_scripts/datarmor_jobs/02_diff_expression/01_trimmomatic_jobs_pe.sh
@@ -4,7 +4,7 @@
 rm 00_scripts/datarmor_jobs/02_diff_expression/TRIM_*sh
 
 # launch scripts for Colosse
-for file in $(ls /home1/scratch/jleluyer/acclimabest/02_data/metabarcoding/*fastq.gz|sed -e 's/_R[12]//g' -e 's/.fastq.gz//'|sort -u)
+for file in $(ls  02_data/diff_expr/*fastq.gz|sed -e 's/_R[12]//g' -e 's/.fastq.gz//'|sort -u)
 
 do
 	base=$(basename "$file")
