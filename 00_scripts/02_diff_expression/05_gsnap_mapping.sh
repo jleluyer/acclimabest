7fd04157de5316cdd312fd517fb4570655095d5d
jleluyer <jeremy.le.luyer@gmail.com>
2018-11-15 17:23:46 -1000

    Update 05_gsnap_mapping.sh

--- 00_scripts/02_diff_expression/05_gsnap_mapping.sh
+++ 00_scripts/02_diff_expression/05_gsnap_mapping.sh
@@ -12,33 +12,16 @@
 
 
 # Global variables
-#DATAOUTPUT="/scratch/home1/jleluyer/acclimabest/04_mapped/diff_expr"
-#DATAINPUT="/scratch/home1/jleluyer/acclimabest/03_trimmed/diff_expr"
-
 DATAOUTPUT="04_mapped/corals"
 DATAINPUT="03_trimmed/corals"
 
 #For combined
-GENOMEFOLDER_combined="/home1/datawork/jleluyer/01_projects/acclimabest/acclimabest/08_trimmed_assembly/"
+GENOMEFOLDER_combined="08_trimmed_assembly/"
 GENOME_combined="combined.transcriptome"
 
-# For transcriptome
-<<<<<<< HEAD
-GENOMEFOLDER_symbiont="/home1/datawork/jleluyer/01_projects/acclimabest/acclimabest/08_trimmed_assembly/"
-GENOME_symbiont="symbiont.transcriptome"
-
-# For genome
-GENOMEFOLDER_host="/home1/datawork/jleluyer/01_projects/acclimabest/acclimabest/08_trimmed_assembly/"
-GENOME_host="host.transcriptome"
-=======
+#for genome C1
 GENOMEFOLDER_symbiont="gmap_symbiodiniumspC1"
 GENOME_symbiont="gmap_symbiodiniumspC1"
-
-# For genome
-GENOMEFOLDER_host="P_margaritifera"
-GENOME_host="indexed_genome"
->>>>>>> 2cb3adf5747abb1060eb5ca4c7d30ac5063238df
-platform="Illumina"
 
 #move to present working dir
 cd $PBS_O_WORKDIR
@@ -69,54 +52,3 @@ base=__BASE__
         samtools sort -n "$DATAOUTPUT"/"$base".combined.bam -o "$DATAOUTPUT"/"$base".combined.sorted.bam
         samtools index "$DATAOUTPUT"/"$base".combined.sorted.bam
 
-
-
-exit
-####################################################################
-################## symbiont ########################################
-####################################################################
-    # Align reads
-    echo "Aligning $base"
-
-    gsnap --gunzip -t 12 -A sam \
-	--dir="$GENOMEFOLDER_symbiont" -d "$GENOME_symbiont" \
-        -o "$DATAOUTPUT"/"$base".symbiont.sam \
-	--max-mismatches=5 --novelsplicing=1 \
-	--read-group-id="$base" \
-	 --read-group-platform="$platform" \
-	"$DATAINPUT"/"$base"_R1.paired.fastq.gz "$DATAINPUT"/"$base"_R2.paired.fastq.gz
-    
-# Create bam file
-    echo "Creating bam for $base"
-    samtools view -Sb -q 5 -F 4 -F 256 \
-        "$DATAOUTPUT"/"$base".symbiont.sam >"$DATAOUTPUT"/"$base".symbiont.bam
-	
-     echo "Creating sorted bam for $base"
-	samtools sort -n "$DATAOUTPUT"/"$base".symbiont.bam -o "$DATAOUTPUT"/"$base".symbiont.sorted.bam
-    	samtools index "$DATAOUTPUT"/"$base".symbiont.sorted.bam
-
-
-####################################################################
-################## host ###########################################
-####################################################################
-
-
-    # Align reads
-    echo "Aligning $base"
-
-    gsnap --gunzip -t 12 -A sam \
-        --dir="$GENOMEFOLDER_host" -d "$GENOME_host" \
-        -o "$DATAOUTPUT"/"$base".host.sam \
-        --max-mismatches=5 --novelsplicing=1 \
-        --read-group-id="$base" \
-         --read-group-platform="$platform" \
-        "$DATAINPUT"/"$base"_R1.paired.fastq.gz "$DATAINPUT"/"$base"_R2.paired.fastq.gz
-
-# Create bam file
-    echo "Creating bam for $base"
-    samtools view -Sb -q 5 -F 4 -F 256 \
-        "$DATAOUTPUT"/"$base".host.sam >"$DATAOUTPUT"/"$base".host.bam
-
-     echo "Creating sorted bam for $base"
-        samtools sort -n "$DATAOUTPUT"/"$base".host.bam -o "$DATAOUTPUT"/"$base".host.sorted.bam
-        samtools index "$DATAOUTPUT"/"$base".host.sorted.bam
