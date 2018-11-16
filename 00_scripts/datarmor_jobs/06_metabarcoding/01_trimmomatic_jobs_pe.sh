7d6863126e64e2c31adbb6b9430d054063291e70
jleluyer <jeremy.le.luyer@gmail.com>
2018-11-15 17:36:43 -1000

    Update 01_trimmomatic_jobs_pe.sh

--- 00_scripts/datarmor_jobs/06_metabarcoding/01_trimmomatic_jobs_pe.sh
+++ 00_scripts/datarmor_jobs/06_metabarcoding/01_trimmomatic_jobs_pe.sh
@@ -4,17 +4,17 @@
 rm 00_scripts/datarmor_jobs/02_diff_expression/TRIM_*sh
 
 # launch scripts for Colosse
-for file in $(ls /home1/scratch/jleluyer/acclimabest/02_data/metabarcoding/*fastq.gz|sed -e 's/_R[12]//g' -e 's/.fastq.gz//'|sort -u)
+for file in $(ls 02_data/metabarcoding/*fastq.gz|sed -e 's/_R[12]//g' -e 's/.fastq.gz//'|sort -u)
 
 do
 	base=$(basename "$file")
-	toEval="cat 00_scripts/02_diff_expression/01_trimmomatic_pe.sh | sed 's/__BASE__/$base/g'"; eval $toEval > 00_scripts/datarmor_jobs/02_diff_expression/TRIM_"$base".sh
+	toEval="cat 00_scripts/06_metabarcoding/01_trimmomatic_pe.sh | sed 's/__BASE__/$base/g'"; eval $toEval > 00_scripts/datarmor_jobs/02_diff_expression/TRIM_"$base".sh
 done
 
 
 #change jobs header
 
 #Submit jobs
-for i in $(ls 00_scripts/datarmor_jobs/02_diff_expression/TRIM_*sh); do qsub $i; done
+for i in $(ls 00_scripts/datarmor_jobs/06_metabarcoding/TRIM_*sh); do qsub $i; done
 
 
